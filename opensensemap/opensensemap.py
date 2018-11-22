7ed7b551022a75641814630826a12d579e69a34c
Stefan Jenkner <stefan@jenkner.org>
2018-11-22 05:23:30 +0100

    Publish temperature and humidity from OpenSenseMap boxes to MQTT.

--- opensensemap/opensensemap.py
+++ opensensemap/opensensemap.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 
-API = 'https://api.opensensemap.org/'
-BOX_ID = '58d42511c877fb0011ad4597'
-
+import argparse
+import logging
 import requests
-import sys
+from time import sleep
+
 import paho.mqtt.client as mqtt
 
-r = requests.get(f"{API}/boxes/{BOX_ID}")
-for s in r.json()['sensors']:
-    if s['sensorType'] in ['HDC1008', 'DHT22']:
-        temp_c = s['lastMeasurement']['value']
-        print(temp_c)
-        client = mqtt.Client('OpenSenseMap')
-        client.connect('raspberrypi.local')
-        client.publish(f'opensensemap/status/{BOX_ID}/temperature', temp_c)
-        client.disconnect()
-        sys.exit()
 
-sys.exit(1)
+def update(box_id):
+    topics = {}
+    try:
+        r = requests.get(f'https://api.opensensemap.org/boxes/{box_id}')
+        for s in r.json()['sensors']:
+            if s['icon'] == 'osem-thermometer':
+                topics[f'opensensemap/status/{box_id}/temperature'] = s['lastMeasurement']['value']
+            elif s['icon'] == 'osem-humidity':
+                topics[f'opensensemap/status/{box_id}/humidity'] = s['lastMeasurement']['value']
+        client = mqtt.Client('opensensemap')
+        client.connect('mosquitto')
+        for topic, payload in topics.items():
+            logging.info(f'Publishing {topic} {payload}')
+            client.publish(topic, payload)
+        client.disconnect()
+    except Exception:
+        logging.error(f'Error updating box: {box_id}')
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('box_id', type=str, nargs='+', help='box id to fetch and publish to MQTT')
+    args = parser.parse_args()
+    while 1:
+        for box_id in args.box_id:
+            update(box_id)
+            sleep(5 * 60)
+
+
+if __name__ == '__main__':
+    main()
