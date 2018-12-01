0fe039799ccd3dd3c28ff72c96f772bb688013ec
Stefan Jenkner <stefan@jenkner.org>
2018-12-01 22:32:12 +0100

    Publish cleaning stats and extended device status to MQTT.

--- mirobo/robo.py
+++ mirobo/robo.py
@@ -8,6 +8,22 @@ import paho.mqtt.client as mqtt
 
 TOPICS = {}
 
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.is_connected=True
+        logging.info('Connected')
+    else:
+        logging.error(f'Connection error, code={rc}')
+
+
+def on_disconnect(client, userdata, rc):
+    client.is_connected=False
+    if rc==0:
+        logging.info('Disconnected')
+    else:
+        logging.error(f'Disconected due to an error, code={rc}')
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
 
@@ -18,23 +34,59 @@ def main():
     info = vac.info()
     device_identifier = info.mac_address.replace(':', '').lower()
 
-    TOPICS['battery'] = f'/mirobo/status/{device_identifier}/battery'
-    TOPICS['sensor_dirt_hours'] = f'/mirobo/status/{device_identifier}/sensor_dirt_hours'
-    TOPICS['sensor_filter_hours'] = f'/mirobo/status/{device_identifier}/sensor_filter_hours'
+    TOPICS['firmware_version'] = f'mirobo/status/{device_identifier}/firmware_version'
+    TOPICS['battery'] = f'mirobo/status/{device_identifier}/battery'
+    TOPICS['fanspeed'] = f'mirobo/status/{device_identifier}/fanspeed'
+    TOPICS['is_on'] = f'mirobo/status/{device_identifier}/is_on'
+    TOPICS['sensor_dirt_hours'] = f'mirobo/status/{device_identifier}/sensor_dirt_hours'
+    TOPICS['filter_hours'] = f'mirobo/status/{device_identifier}/filter_hours'
+    TOPICS['main_brush_hours'] = f'mirobo/status/{device_identifier}/main_brush_hours'
+    TOPICS['side_brush_hours'] = f'mirobo/status/{device_identifier}/side_brush_hours'
+    TOPICS['cleaning_summary_count'] = f'mirobo/status/{device_identifier}/cleaning_summary_count'
+    TOPICS['cleaning_summary_total_area'] = f'mirobo/status/{device_identifier}/cleaning_summary_total_area'
+    TOPICS['cleaning_summary_total_hours'] = f'mirobo/status/{device_identifier}/cleaning_summary_total_hours'
 
+    mqtt.Client.is_connected=False
     client = mqtt.Client('mirobo')
+    client.on_connect=on_connect
+    client.on_disconnect=on_disconnect
     client.loop_start()
     client.connect('mosquitto', 1883, 60)
     while True:
         try:
+            # device info
+            publish(client, TOPICS['firmware_version'], info.firmware_version)
+
+            # status
             status = vac.status()
+            publish(client, TOPICS['battery'], float(status.battery))
+            publish(client, TOPICS['fanspeed'], status.fanspeed)
+            publish(client, TOPICS['is_on'], status.is_on)
+
+            # consumables
             consumable_status = vac.consumable_status()
-            client.publish(TOPICS['battery'], status.battery)
-            client.publish(TOPICS['sensor_dirt_hours'], float(consumable_status.sensor_dirty.seconds / 60 / 60))
-            client.publish(TOPICS['sensor_filter_hours'], float(consumable_status.filter.seconds / 60 / 60))
+            publish(client, TOPICS['sensor_dirt_hours'], float(consumable_status.sensor_dirty.total_seconds() / 3600))
+            publish(client, TOPICS['filter_hours'], float(consumable_status.filter.total_seconds() / 3600))
+            publish(client, TOPICS['main_brush_hours'], float(consumable_status.main_brush.total_seconds() / 3600))
+            publish(client, TOPICS['side_brush_hours'], float(consumable_status.side_brush.total_seconds() / 3600))
+
+            # cleaning stats
+            clean_history = vac.clean_history()
+            publish(client, TOPICS['cleaning_summary_count'], float(clean_history.count))
+            publish(client, TOPICS['cleaning_summary_total_area'], float(clean_history.total_area))
+            publish(client, TOPICS['cleaning_summary_total_hours'], float(clean_history.total_duration.total_seconds() / 3600))
         except:
             logging.error("Error getting device status")
         sleep(5*60)
+
+
+def publish(client, topic, payload):
+    while not client.is_connected:
+        logging.info('Waiting for connection')
+        sleep(1)
+    logging.info(f'Publishing {topic} {payload}')
+    client.publish(topic, str(payload))
+
 
 if __name__ == '__main__':
     main()
